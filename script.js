133717bef07c8067fabaa9249c38b4908e71d23f
Danielle Lindblom <danielle.lindblom@gmail.com>
2022-12-08 10:48:14 -0700

    images appear on click

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// declare variables
 const pythonCertificate = document.getElementById('python_cert');
 const intermediatePythonCertificate = document.getElementById('intermediate_python_cert');
 const htmlCertificate = document.getElementById('html_cert');
@@ -6,10 +5,15 @@ const cssCertificate = document.getElementById('css_cert');
 const intermediateCSSCertificate = document.getElementById('intermediate_css_cert');
 const javascriptCertificate = document.getElementById('javascript_cert');
 
+const pythonLink = document.getElementById('python');
+const intermediatePythonLink = document.getElementById('intermediate_python');
+const htmlLink = document.getElementById('html');
+const cssLink = document.getElementById('css');
+const intermediateCSSLink = document.getElementById('intermediate_css');
+const javascriptLink = document.getElementById('javascript');
 
-// set all certificates to hidden as default
+
 window.onload = (event) => {
-
     pythonCertificate.hidden = true;
     intermediatePythonCertificate.hidden = true;
     htmlCertificate.hidden = true;
@@ -18,19 +22,23 @@ window.onload = (event) => {
     javascriptCertificate.hidden = true;
 }
 
-// create functions
-const showCertificate = (element) => {
-    element.hidden = false;
-}
 
-const hideCertificate = (element) => {
-    element.hidden = true;
-}
+pythonLink.addEventListener("click", () => {
+    pythonCertificate.hidden = false;
+});
+intermediatePythonLink.addEventListener("click", () => {
+    intermediatePythonCertificate.hidden = false;
+});
+htmlLink.addEventListener("click", () => {
+    htmlCertificate.hidden = false;
+});
+cssLink.addEventListener("click", () => {
+    cssCertificate.hidden = false;
+});
+intermediateCSSLink.addEventListener("click", () => {
+    intermediateCSSCertificate.hidden = false;
+});
+javascriptLink.addEventListener("click", () => {
+    javascriptCertificate.hidden = false;
+});
 
-// create event listeners
-pythonCertificate.addEventListener("click", showCertificate);
-intermediatePythonCertificate.addEventListener("click", showCertificate);
-htmlCertificate.addEventListener("click", showCertificate);
-cssCertificate.addEventListener("click", showCertificate);
-intermediateCSSCertificate.addEventListener("click", showCertificate);
-javascriptCertificate.addEventListener("click", showCertificate);
