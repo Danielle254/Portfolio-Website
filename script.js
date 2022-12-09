4858472b45a77567152e26c88950028d2281b2ce
mdwiltfong <wiltfong.michael1@gmail.com>
2022-12-08 23:55:05 -0500

    dynamical generates HTML elements

--- script.js
+++ script.js
@@ -1,97 +1,43 @@
+/*
+This line of code imports or brings in the certificate.json file. 
+*/
 import certificates from "./Resources/certificates.json" assert { type: "json" };
 console.log(certificates);
 const certContainer = document.getElementsByClassName(
   "certificateContainer"
 )[0];
-
+/*
+appendCertificates is a function type called Immediately Invoked Function Expression (IIFE).
+This means that it's invoked as soon as it's defined. This function adds paragraph tags, but it also adds 
+image tags with the "cert_img" class. This allows us to pick up the image tags later on. 
+*/
 (function appendCertificates() {
   let stringOutput = "";
   certificates.forEach((certificate) => {
     stringOutput += `<p class="certificate" id=${certificate.id}>${certificate.inner_html}</p>
-        <img src=${certificate.src} id=${certificate.img_id} alt=${certificate.alt}>`;
+        <img src=${certificate.src} class="cert_img" id=${certificate.img_id} alt=${certificate.alt}>`;
   });
   certContainer.innerHTML = stringOutput;
 })();
-
-const pythonCertificate = document.getElementById("python_cert");
-const intermediatePythonCertificate = document.getElementById(
-  "intermediate_python_cert"
-);
-const htmlCertificate = document.getElementById("html_cert");
-const cssCertificate = document.getElementById("css_cert");
-const intermediateCSSCertificate = document.getElementById(
-  "intermediate_css_cert"
-);
-const javascriptCertificate = document.getElementById("javascript_cert");
-
-const pythonLink = document.getElementById("python");
-const intermediatePythonLink = document.getElementById("intermediate_python");
-const htmlLink = document.getElementById("html");
-const cssLink = document.getElementById("css");
-const intermediateCSSLink = document.getElementById("intermediate_css");
-const javascriptLink = document.getElementById("javascript");
-
-var pythonTimesClicked = 0;
-var intermediatePythonTimesClicked = 0;
-var htmlTimesClicked = 0;
-var cssTimesClicked = 0;
-var intermediateCSSTimesClicked = 0;
-var javascriptTimesClicked = 0;
-
+// Grab all the certificate images that have been appended a
+const certs = document.querySelectorAll(".cert_img");
+console.log(certs);
+// Set the HTML hidden attribute to true
 window.onload = (event) => {
-  pythonCertificate.hidden = true;
-  intermediatePythonCertificate.hidden = true;
-  htmlCertificate.hidden = true;
-  cssCertificate.hidden = true;
-  intermediateCSSCertificate.hidden = true;
-  javascriptCertificate.hidden = true;
+  certs.forEach((cert) => (cert.hidden = true));
 };
+/* 
+Through a feature called "event bubbling" we can capture any clicks made on the certContainer.
+The function also makes sure that any target being passed in has the "certificate" class. That way we can assure what element is being clicked. 
+It then switches the state of the hidden attribute of the HTML element being sent to the function.
 
-pythonLink.addEventListener("click", () => {
-  pythonTimesClicked++;
-  if (pythonTimesClicked % 2 === 1) {
-    pythonCertificate.hidden = false;
-  } else {
-    pythonCertificate.hidden = true;
-  }
-});
-intermediatePythonLink.addEventListener("click", () => {
-  intermediatePythonTimesClicked++;
-  if (intermediatePythonTimesClicked % 2 === 1) {
-    intermediatePythonCertificate.hidden = false;
-  } else {
-    intermediatePythonCertificate.hidden = true;
-  }
-});
-htmlLink.addEventListener("click", () => {
-  htmlTimesClicked++;
-  if (htmlTimesClicked % 2 === 1) {
-    htmlCertificate.hidden = false;
-  } else {
-    htmlCertificate.hidden = true;
-  }
-});
-cssLink.addEventListener("click", () => {
-  cssTimesClicked++;
-  if (cssTimesClicked % 2 === 1) {
-    cssCertificate.hidden = false;
-  } else {
-    cssCertificate.hidden = true;
-  }
-});
-intermediateCSSLink.addEventListener("click", () => {
-  intermediateCSSTimesClicked++;
-  if (intermediateCSSTimesClicked % 2 === 1) {
-    intermediateCSSCertificate.hidden = false;
-  } else {
-    intermediateCSSCertificate.hidden = true;
-  }
-});
-javascriptLink.addEventListener("click", () => {
-  javascriptTimesClicked++;
-  if (javascriptTimesClicked % 2 === 1) {
-    javascriptCertificate.hidden = false;
-  } else {
-    javascriptCertificate.hidden = true;
+*/
+certContainer.addEventListener("click", (e) => {
+  console.log(e);
+  const eventTarget = e.target;
+  const className = eventTarget.getAttribute("class");
+  if (className == "certificate") {
+    const hiddenState = eventTarget.nextElementSibling.hidden;
+    eventTarget.nextElementSibling.hidden = !hiddenState;
   }
 });
